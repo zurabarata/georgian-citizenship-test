51a450fb1ec63dbd155473a86ee15b4aa237b4b3
Zurab Baratashvili <zurab@candis.io>
2025-08-16 17:37:17 +0400

    fix issues

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from '@testing-library/react';
 import {App} from './App';
+import {describe, it, expect} from 'vitest';
 
 // Simple test to check if app renders
 describe('App', () => {
